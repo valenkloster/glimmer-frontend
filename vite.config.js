7559d2be4e075e873b551d085848cec1280bc889
valenkloster <valekloster18@gmail.com>
2025-02-02 21:02:34 -0300

    chore: update Vite configuration to allow external access and set default port; specify output directory for build

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT || 5173,
+    host: '0.0.0.0', // Añade esta línea
+    port: process.env.PORT || 10000, // Cambia el puerto
   },
+  preview: {
+    host: '0.0.0.0', // Añade esta configuración para preview
+    port: process.env.PORT || 10000, // Mismo puerto
+  },
+  build: {
+    outDir: 'dist' // Añade esta línea para especificar el directorio de salida
+  }
 })
-
